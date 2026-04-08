fc611dd0b0a7ed3dfbe9ebdcb2b7cb0198fa9097
JamesPC2005 <jamespconyt@gmail.com>
2026-04-08 13:20:42 -0700

    added a copy of professors work in class, fist local server up and runninv

---
+++ portfolio-James-Cunningham/app.js
@@ -0,0 +1,19 @@
+// console.log("Start");
+
+// setTimeout(()=>{console.log("inside timeout")},2000);
+
+// console.log("end")
+
+const http = require("http");
+const fs = require("fs");
+
+const server = http.createServer((req, res) =>
+    fs.readfile("index.html", (err,data)=>{
+        res.writehead( 200,{"content-Type":"text/html"});
+        res.write(data);
+        res.end();
+}));
+
+server.listen(3000, () => {
+    console.log("server running on https//localhost:3000")
+});
