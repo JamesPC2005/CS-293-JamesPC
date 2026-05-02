ef95d49af5ee568d18185567f109d1e973a3b661
Robert Cunningham <jamespconyt@gmail.com>
2026-05-01 19:55:05 -0700

    got the js working by using the open class trick like in the zybooks. made the html a bit more readable. now all i have to do is make sure i can localhost it

--- portfolio-James-Cunningham/Bells-and-Whistles.js
+++ portfolio-James-Cunningham/Bells-and-Whistles.js
@@ -6,11 +6,18 @@ document.addEventListener("DOMContentLoaded", function () {
   dropdown.addEventListener("click", change_dropdown);
 });
 
-const fill = null
+console.log("js loading")
+function change_dropdown() {
+    const target = document.getElementById("target");
+    const dropdown = document.getElementById("drop-down")
+    /*console.log("click")*/
 
-function change_dropdown(event) {
-    let target = document.getElementById("target");
-    if(target.innerHTML === ""){
-        target.innerHTML = '<li><p>My 2025-2026 <a href="https://github.com/JamesPC2005/CS-293-JamesPC">web development</a>work for class</p></li><li><p>Some of my<a href="https://github.com/JamesPC2005/personal_projects">personal projects</a>from 2024-2025 </p></li>'
+    if (target.classList.contains("open")) {
+        dropdown.innerHTML=(`My Projects <`)
+        target.classList.remove("open");
+
+    } else {
+        dropdown.innerHTML=(`My Projects V`);
+        target.classList.add("open");
     }
 }
--- portfolio-James-Cunningham/app.js
+++ portfolio-James-Cunningham/app.js
@@ -3,25 +3,6 @@
 // setTimeout(()=>{console.log("inside timeout")},2000);
 
 // console.log("end")
-document.addEventListener("DOMContentLoaded", function () {
-  
-  const dropdown = document.getElementById("drop-down");
-
-  
-  dropdown.addEventListener("click", change_dropdown);
-});
-
-const fill = null
-
-function change_dropdown(event) {
-    let target = document.getElementById("target");
-    if(target.innerHTML === ""){
-        target.innerHTML = '<li><p>My 2025-2026 <a href="https://github.com/JamesPC2005/CS-293-JamesPC">web development</a>work for class</p></li><li><p>Some of my<a href="https://github.com/JamesPC2005/personal_projects">personal projects</a>from 2024-2025 </p></li>'
-    }
-}
-
-
-
 
 const http = require("http");
 const fs = require("fs");
