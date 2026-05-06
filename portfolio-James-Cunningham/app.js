5b5e4712f4432ac4c28c91a2f25367e72fde296c
Robert Cunningham <jamespconyt@gmail.com>
2026-05-05 22:38:52 -0700

    added finishing designs to most of the doccument, along with filling the text of the different sections

--- portfolio-James-Cunningham/app.js
+++ portfolio-James-Cunningham/app.js
@@ -6,14 +6,39 @@
 
 const http = require("http");
 const fs = require("fs");
+const path = require("path")
+
 
 const server = http.createServer((req, res) =>{
-    fs.readFile("index.html", (err,data)=>{
-        res.writeHead( 200,{"Content-Type":"text/html"});
-        res.write(data);
-        res.end();
-    })
-    
+
+    let filepath = req.url;
+
+    if(filepath==="/"){
+        filepath = "/index.html"
+    }
+
+    filepath = path.join(__dirname, filepath);
+
+    const ext = path.extname(filepath);
+
+    let content="text/plain";
+
+    if(ext === ".html"){content = "text/html"};
+    if(ext === ".css"){content = "text/css"};
+    if(ext === ".js"){content = "text/javascript"};
+
+    fs.readFile(filepath, (err,data)=>{
+
+        if (err) {
+            res.writeHead(404, { "Content-Type": "text/plain" });
+            res.end("Not found");
+            return;
+        }
+
+        res.writeHead(200, { "Content-Type": content });
+        res.end(data);
+    });
+
 });
 
 server.listen(3000, () => {
